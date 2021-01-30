7b8f85946857d301cbd4017208370162cacdd771
smartman1234 <john.smith.dev0724@gmail.com>
2021-01-30 23:55:33 +0100

    importing the font links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,14 @@ export default class CustomDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
           />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300&display=swap"
+            rel="stylesheet"
+          />
+         <link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
         </Head>
 
         <body>
