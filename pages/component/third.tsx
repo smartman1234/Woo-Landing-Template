c1b498d4333a9359dba49043a57c0731e5e2765b
smartman1234 <john.smith.dev0724@gmail.com>
2021-01-30 20:11:10 +0100

    creating a third page and css file

---
+++ pages/component/third.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import styles from './third.module.scss';
+
+const Secondpage = () => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.bold}>
+        <h3>Develop Without Limits</h3>
+      </div>
+    </main>
+  );
+};
+
+export default Secondpage;
