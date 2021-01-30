d7d8a37e6970d7c0bd4b39d70197361a247c1923
smartman1234 <john.smith.dev0724@gmail.com>
2021-01-30 20:12:02 +0100

    Set up for secod page

--- pages/component/second.tsx
+++ pages/component/second.tsx
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
 import styles from './first.module.scss';
 
-const FirstPage = () => {
+const Secondpage = () => {
   return (
-    <main className={styles.first}>
-      <div className={styles.left}>
-        <h4>Building exactly the eCommerce website you want.</h4>
-        <p>
-          WooCommerce is a customizable, open-source eCommerce platform built on
-          WordPress. Get started quickly and make your way.
-        </p>
-        <button className={styles.btn}>Start a New Store</button>
-        <span>or</span>
-        <a href="/">Customize & Extend ›</a>
+    <main className={styles.relative}>
+      <div className={styles.bold}>
+        <h2>Your eCommerce made simple</h2>
       </div>
-      <div className={styles.right}>
-        <img src="/back.png" alt="side image" className={styles.back} />
-        <img
-          src="/featured-image.png"
-          alt="side image"
-          className={styles.top}
-        />
-      </div>
+      <article className={styles.sec}>
+        <div className={styles.box1}>
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <h3>All You Need to Start</h3>
+          <a href="/">Ecommerce for Wordpress ›</a>
+        </div>
+        <div className={styles.box2}>
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <h3>Customize and Extend</h3>
+          <p>
+            From subscriptions to gym classes to luxury cars, WooCommerce is
+            fully customizable.
+          </p>
+          <a href="/">Browse Extensions ›</a>
+        </div>
+        <div className={styles.box3}>
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <img src="/" alt="" />
+          <h3>Active Community</h3>
+          <p>
+            WooCommerce is one of the fastest-growing eCommerce communities.{' '}
+          </p>
+          <a href="/">Check our Forums ›</a>
+        </div>
+      </article>
     </main>
   );
 };
 
-export default FirstPage;
+export default Secondpage;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import styles from '../styles/Home.module.css';
 import Nav from './component/nav';
 import Footer from './component/footer'
 import First from './component/first';
+import Second from './component/second';
+import Third from './component/third';
 export default function Home() {
   return (
     <div className={styles.container}>
       <Nav />
-<First/>
+      <First />
+      <Second />
+      <Third/>
       <main className={styles.main}></main>
 
       <Footer />
