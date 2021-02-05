d78c619f72a03e1266904cd825cf28bab5b101e8
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-05 18:55:25 +0100

    positioning images

--- pages/main/second.tsx
+++ pages/main/second.tsx
@@ -25,13 +25,21 @@ const Secondpage = () => {
             <img src="/Rectangle 223.png" alt="" className={styles.mainimg} />
             <div className={styles.content}>
               <h3 className={styles.heads}>All You Need to Start</h3>
+              <p className={styles.writing}>
+                Add WooCommerce plugin to any WordPress site and set up a new
+                store in minutes.
+              </p>
               <a href="/">Ecommerce for Wordpress ›</a>
             </div>
           </div>
           <div className={styles.box2}>
             <img src="" alt="" />
             <img src="/Rectangle 224.png" alt="" className={styles.mainimg} />
-            <img src="/" alt="" />
+            <img src="/image 17.png" alt="" className={styles.topright} />
+            <img src="/image 18.png" alt="" className={styles.img3} />
+            <img src="/image 19.png" alt="" className={styles.img2} />
+            <img src="/image 20.png" alt="" className={styles.img5} />
+            <img src="/image 22.png" alt="" className={styles.img4} />
             <div className={styles.content}>
               <h3 className={styles.heads}>Customize and Extend</h3>
               <p className={styles.writing}>
@@ -44,7 +52,9 @@ const Secondpage = () => {
           <div className={styles.box3}>
             <img src="/" alt="" />
             <img src="/Rectangle 225.png" alt="" className={styles.mainimg} />
-            <img src="/" alt="" />
+            <img src="/Rectangle 226" alt="" className={styles.leftimg} />
+            <img src="/Rectangle 227" alt="" className={styles.topimg} />
+            <img src="/Rectangle 228" alt="" className={styles.outsideimg} />
             <div className={styles.content}>
               <h3 className={styles.heads}>Active Community</h3>
               <p className={styles.writing}>
