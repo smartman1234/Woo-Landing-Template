bb1abfe3201615db12d2736a644d24469037050e
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-12 19:15:43 +0100

    importing  font family link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class CustomDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
           />
+
           <link
             href="//db.onlinewebfonts.com/c/961a181da27e7cbc072ec2fb5bbfe2a9?family=Circular+Std+Bold"
             rel="stylesheet"
@@ -21,6 +22,11 @@ export default class CustomDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
             rel="stylesheet"
           />
         </Head>
