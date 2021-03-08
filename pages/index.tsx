cdfa8e904b7320b879587787bb9b41eed32732c1
smartman1234 <john.smith.dev0724@gmail.com>
2021-03-09 00:08:46 +0100

    removed white spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-
 import First from '../component/first/first';
 import Second from '../component/first/second';
 import Third from '../component/third/third';
